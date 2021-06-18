6e5ca0ad12c0ed2924430551a2b1cf8a966881bc
liuzuncai <liuzuncai@enn.cn>
2021-06-18 10:44:18 +0800

    提交工具方法

--- src/main/java/com/example/demodata/controller/AopController.java
+++ src/main/java/com/example/demodata/controller/AopController.java
@@ -24,4 +24,27 @@ public class AopController {
         aopTestImpl.save("胡道权",23);
     }
 
+    public static void main(String[] args) {
+
+        //== 的作用：
+        //　　基本类型：比较值是否相等
+        //　　引用类型：比较内存地址值是否相等
+        //
+        //equals 的作用:
+        //　　引用类型：默认情况下，比较内存地址值是否相等。可以按照需求逻辑，重写对象的equals方法。
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+    }
+
 }
